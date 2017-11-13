44b7280e3ad354ad675d91a4e2007ace7ef20a64
Veerender <vchama@cisco.com>
2017-11-12 22:11:57 -0800

    Implemented redemtption processing logic

--- data/mfObj.js
+++ data/mfObj.js
@@ -2,26 +2,33 @@ var mfProfile = {
 	"100033": {
 		"name": "Aditya Birla Sun Life Advantage Fund - Regular Growth",
 		"purchase": [{
-			"units": 10.06,
-			"nav": 95.55,
+			"units": 10,
+			"nav": 95,
 			"date": "27-Oct-2017"
 		}, {
-			"units": 10.15,
-			"nav": 90.25,
+			"units": 10,
+			"nav": 90,
 			"date": "27-Nov-2017"
 		}, {
-			"units": 25.86,
-			"nav": 92.15,
+			"units": 25,
+			"nav": 92,
 			"date": "28-Nov-2017"
 		}],
 		"redemption": [{
-			"units": 3.15,
-			"nav": 98.07,
+			"units": 20,
+			"nav": 90,
 			"date": "30-Oct-2017"
 		}, {
-			"units": 10.97,
-			"nav": 90.85,
+			"units": 10,
+			"nav": 90,
 			"date": "30-Nov-2017"
+		}],
+		"dividend":[{
+			"rate":2.5,
+			"date":"20-Nov-2017"
+		},{
+			"rate":0.5,
+			"date":"25-Nov-2017"
 		}]
 	}
 
@@ -29,7 +36,7 @@ var mfProfile = {
 //Determines Avg Cost price of the Investments made at different intervals
 function avgCP(mfObj) {
 	console.log("Average cost price of MF ", mfObj["name"]);
-	var unitsRedeemed = 0;
+	/*var unitsRedeemed = 0;	
 	if (typeof mfObj !== "undefined") {
 		if ((mfObj["redemption"] !== "undefined") && (mfObj["redemption"].length > 0)) {
 			var redemptArr = mfObj["redemption"];
@@ -38,26 +45,71 @@ function avgCP(mfObj) {
 			}
 		}
 		console.log("Total Units Redeemed:", unitsRedeemed);
-		var avgCPObj = processRedemption(mfObj,unitsRedeemed);
+	*/
+		var avgCPObj = processRedemption(mfObj);
 		console.log("Balance Units: "+avgCPObj["totalUnits"]);
 		console.log("Amount Costed: "+avgCPObj["totalCostAmt"]);
+		console.log("Amount Realized: "+avgCPObj["realized"]);
+	}
+
+// function process and returns the redemption details
+function getRedemptionData(mfObj){
+	var unitsRedeemed = 0;
+	var redemptionAmt = 0;
+	if (typeof mfObj !== "undefined") {
+		if ((mfObj["redemption"] !== "undefined") && (mfObj["redemption"].length > 0)) {
+			var redemptArr = mfObj["redemption"];
+			for (var i = 0; i < redemptArr.length; i++) {
+				unitsRedeemed = redemptArr[i]["units"] + unitsRedeemed;
+				redemptionAmt = (redemptArr[i]["units"]*redemptArr[i]["nav"])+redemptionAmt;
+			}
+		}
+	}
+	return{
+		"unitsRedeemed":unitsRedeemed,
+		"redemptionAmt":redemptionAmt
 	}
 }
-function processRedemption(mfObj, unitsRedeemed) {
+//function process and returns dividend data
+function getDividendData(mfObj){
+	
+}
+
+function processRedemption(mfObj) {
+	var redempDataObj = getRedemptionData(mfObj);	
+	var unitsRedeemed = redempDataObj["unitsRedeemed"];
+	var redemptionAmt = redempDataObj["redemptionAmt"];
+	console.log("unitsRedeemed: "+unitsRedeemed+" RedemptionAmount: "+redemptionAmt);
+
 	if ((mfObj["purchase"] !== "undefined") && (mfObj["purchase"].length > 0)) {
 		var purchaseArr = mfObj["purchase"];
 		var totalUnits = 0;
 		var totalCostAmt = 0;
+		var realized = 0;
 		for (var j = 0; j < purchaseArr.length; j++) {
 			if (unitsRedeemed == 0) {
 				totalUnits = totalUnits + purchaseArr[j]["units"];
 				totalCostAmt = totalCostAmt + (purchaseArr[j]["units"] * purchaseArr[j]["nav"]);
-			} else if ((purchaseArr[j]["units"] - unitsRedeemed) > 0) {
+			} else if ((purchaseArr[j]["units"] - unitsRedeemed) >= 0) {
+				if(redemptionAmt === 0){
+					realized = (unitsRedeemed*purchaseArr[j]["nav"])+realized;					
+				}else{
+					realized = (unitsRedeemed*purchaseArr[j]["nav"])-redemptionAmt;
+				}				
 				var balanceUnits = purchaseArr[j]["units"] - unitsRedeemed;
 				totalUnits = totalUnits + balanceUnits;
 				totalCostAmt = totalCostAmt + (balanceUnits * purchaseArr[j]["nav"]);
 				unitsRedeemed = 0;
 			} else if ((purchaseArr[j]["units"] - unitsRedeemed) < 0) {
+				if(realized<0 || realized>0){
+					realized = (purchaseArr[j]["units"]*purchaseArr[j]["nav"])+realized;
+					console.log("realized",realized)
+				}else{
+					realized = (purchaseArr[j]["units"]*purchaseArr[j]["nav"])-redemptionAmt;
+					console.log("realized",realized);
+					redemptionAmt =0; // since it is already in realized, marking it to zero
+				}
+				
 				unitsRedeemed = -(purchaseArr[j]["units"] - unitsRedeemed);
 			}
 
@@ -65,7 +117,8 @@ function processRedemption(mfObj, unitsRedeemed) {
 	}
 	return {
 		"totalUnits": totalUnits,
-		"totalCostAmt": totalCostAmt
+		"totalCostAmt": totalCostAmt,
+		"realized": -(realized)
 	}
 }
 //Determines the profit or loss realized
